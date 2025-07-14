9edefd841abf22bfb09379f7ee395634da314763
Emily Suriel <emilysuriel0@gmail.com>
2025-07-14 10:16:17 -0400

    Changed inital veiw added another view to save our task.

--- ToDoListApp/ContentView.swift
+++ ToDoListApp/ContentView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            
+            HStack{
+                Text("To-Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Spacer()
+                
+                Button{
+                    
+                    
+                }//closing button
+                label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                } //closing label
+                
+            } //closing HStack
+            
+        }//closing VStack
         .padding()
+        Spacer()
     }
 }
 
