18a42a00efb74f077fcfd3436bdd9b8590cf54a2
Emily Suriel <emilysuriel0@gmail.com>
2025-07-14 09:58:22 -0400

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Indiana Suriel on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
